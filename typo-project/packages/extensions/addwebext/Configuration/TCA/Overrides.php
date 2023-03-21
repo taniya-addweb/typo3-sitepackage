9af1863e48d2df8974aeac28f63dae6da454131b
taniya-addweb <119279726+taniya-addweb@users.noreply.github.com>
2023-03-21 19:25:21 +0530

    Delete typo-project/packages/extensions/addwebext directory

--- typo-project/packages/extensions/addwebext/Configuration/TCA/Overrides.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die();
-
-$GLOBALS['TCA']['tt_content']['types']['taniya'] = [    'showitem' => '
-        --palette--;;general,
-        header;Header,
-        bodytext;Text,
-    '];
-?>
